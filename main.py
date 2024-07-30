c06d6f2c6d86473ae8d0d83ad3d22fbc6001f1e4
Phinatech <ugbojudithchinemerem@gmail.com>
2024-07-30 14:02:21 +0200

    pushing

--- main.py
+++ main.py
@@ -1,91 +1,4 @@
-# from grade_book import GradeBook
 
-# grade_book = GradeBook()
-
-# def display_menu():
-#     print("Welcome to Chinemerem's Grade Book Application!")
-#     print("Please choose an action:")
-#     print("1. Add a student")
-#     print("2. Add a course")
-#     print("3. Register a student for a course")
-#     print("4. Calculate student ranking")
-#     print("5. Search by grade")
-#     print("6. Generate transcript")
-#     print("7. Print list of students")
-#     print("8. Print list of course")
-#     print("9. Exit")
-
-# while True:
-#     display_menu()
-#     choice = input("Enter your choice (1-9): ")
-
-#     if choice == "1":
-#         email = input("Enter student email: ")
-#         names = input("Enter student names: ")
-#         grade_book.add_student(email, names)
-
-#     elif choice == "2":
-#         name = input("Enter course name: ")
-#         trimester = input("Enter course trimester: ")
-#         credits = int(input("Enter course credits: "))
-#         grade_book.add_course(name, trimester, credits)
-
-#     elif choice == "3":
-#         student_email = input("Enter student email: ")
-#         course_name = input("Enter course name: ")
-#         grade_book.register_student_for_course(student_email, course_name)
-
-#     elif choice == "4":
-#         grade_book.calculate_ranking()
-#         print("Student ranking calculated successfully.")
-
-#     elif choice == "5":
-#         min_grade = float(input("Enter minimum grade: "))
-#         max_grade = float(input("Enter maximum grade: "))
-#         students = grade_book.search_by_grade(min_grade, max_grade)
-#         # print(students)
-#         if students:
-#             print("Students found:")
-#             for student in students:
-#                 print(f"Name: {students.names}, Email: {students.email}")
-#         else:
-#             print("No students found within the specified grade range.")
-
-#     elif choice == "6":
-#         email = input("Enter student email: ")
-#         student = next(
-#             (s for s in grade_book.student_list if s.email == email), None)
-#         if student:
-#             grade_book.generate_transcript(student)
-#         else:
-#             print("Student not found.")
-
-#     elif choice == "7":
-#         print("Printing student list...")
-#         print('----------------------------------------------------')
-#         for student in grade_book.student_list:
-#             print(f"Name: {student.names}", f"Email: {student.email}")
-#         print('----------------------------------------------------')
-
-#     elif choice == "8":
-#         print("Printing course list...")
-#         print('----------------------------------------------------')
-#         for course in grade_book.course_list:
-#             print(f"Name: {course.name}", f"Trimester: {course.trimester}")
-#         print('----------------------------------------------------')
-
-#     elif choice == "9":
-#         print("Exiting Grade Book Application...")
-#         break
-
-#     else:
-#         print("Invalid choice. Please try again.")
-
-#     # Ask if the user wants to perform another action
-#     continue_choice = input("Do you want to perform another action? (yes/no): ").strip().lower()
-#     if continue_choice != 'yes':
-#         print("Exiting Grade Book Application...")
-#         break
 
 from grade_book import GradeBook
 import re
@@ -189,9 +102,9 @@ while True:
     while True:
         continue_choice = input("Do you want to perform another action? (yes/no): ").strip().lower()
         if continue_choice == 'yes':
-            break  # Exit the inner loop to perform another action
+            break  
         elif continue_choice == 'no':
             print("Exiting Grade Book Application...")
-            exit()  # Exit the entire program
+            exit() 
         else:
             print("Invalid response. Please enter 'yes' or 'no'.")
--- student.py
+++ student.py
@@ -1,53 +1,4 @@
-# #!/usr/bin/python3
-# from course import Course
-# import random
 
-
-# class Student:
-#     def __init__(self, email, names, courses_registered=None, GPA=0.0):
-#         """
-#         Initialize a new Student object.
-
-#         Args:
-#             email (str): The email of the student.
-#             names (str): The names of the student.
-#         """
-#         self.email = email
-#         self.names = names
-#         self.courses_registered = courses_registered if courses_registered is not None else []
-#         self.GPA = GPA
-
-#     def register_for_course(self, course):
-#         """
-#         Register the student for a course.
-
-#         Args:
-#             course (dict): A dictionary with course details (name, credits, grade).
-#         """
-#         grade = random.randint(40, 100)
-#         self.courses_registered.append({
-#             'name': course.name,
-#             'trimester': course.trimester,
-#             'credits': course.credits,
-#             'grade': grade
-#         })
-#         # self.calculate_GPA()
-
-#     def calculate_GPA(self):
-#         total_credits = 0
-#         total_grade_points = 0
-#         for course in self.courses_registered:
-            
-#             # casted_course = Course(
-#             #     course['name'], course['trimester'], course['credits'])
-#             total_credits += course['credits']
-#             total_grade_points += course['credits'] * course['grade']
-#         GPA = total_grade_points / total_credits if total_credits else 0.0
-#         return (GPA/100)*5
-        
-
-
-    #!/usr/bin/python3
 from course import Course
 import random
 
@@ -80,15 +31,13 @@ class Student:
             'credits': course.credits,
             'grade': grade
         })
-        # self.calculate_GPA()
+        
 
     def calculate_GPA(self):
         total_credits = 0
         total_grade_points = 0
         for course in self.courses_registered:
-            
-            # casted_course = Course(
-            #     course['name'], course['trimester'], course['credits'])
+
             total_credits += course['credits']
             total_grade_points += course['credits'] * course['grade']
         GPA = total_grade_points / total_credits if total_credits else 0.0
